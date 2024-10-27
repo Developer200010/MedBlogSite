22c2abdc9f3e02f0dee8dfb9aaa66afaf637d7ad
chandan vishwakarma <chandanvishw11002@gmail.com>
2024-10-27 14:49:02 +0530

    build

--- frontend/src/pages/write/Write.jsx
+++ frontend/src/pages/write/Write.jsx
@@ -1,53 +1,66 @@
 import { useContext, useState } from "react";
 import "./write.css";
-import axios from "axios"
-import {Context} from "../../context/Context"
+import axios from "axios";
+import { Context } from "../../context/Context";
 
 export default function Write() {
-  const [title, setTitle] = useState("")
-  const [desc, setDesc] = useState("")
-  const [file, setFile] = useState(null)
-  const {user} = useContext(Context)
-  const [error, setError] = useState(false)
-  console.log(user.username)
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+  const [error, setError] = useState(false);
+
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    try {
-      const res = await axios.post("/api/post",{
+      e.preventDefault();
+      const newPost = {
         username: user.username,
-        title : title,
-        desc : desc
-      })
-      res.data && window.location.replace("/post/" + res.data._id);
-      console.log(res.data)
-    } catch (error) {
-      console.log(error.message)
-      setError(true)
-    }
-  
-  }
+        title,
+        desc,
+      };
+      if (file) {
+        const data =new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name", filename);
+        data.append("file", file);
+        newPost.photo = filename;
+        try {
+          await axios.post("/upload", data);
+        } catch (error) {
+          console.log(error.message)
+        }
+      }
+      try {
+        const res = await axios.post("/post", newPost);
+        window.location.replace("/post/" + res.data._id);
+      } catch (err) {
+        setError(true)
+      }}
+
   return (
     <div className="write">
-          <img
-          className="writeImg"
-          // src={URL.createObjectURL(file)}
-          src="https://images.pexels.com/photos/27971523/pexels-photo-27971523/free-photo-of-a-mountain-hut-overlooks-a-spectacular-valley-in-the-swiss-alps.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
-        
-   
+        {file && (
+        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+      )}
+
       <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label className="img" htmlFor="fileInput">
             <h5>select Image</h5>
           </label>
-          <input id="fileInput" className="file1" type="file" style={{ display: "none" }} onChange={e=> setFile(e.target.files[0])} />
+          <input
+            id="fileInput"
+            className="file1"
+            type="file"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+            name="file"
+          />
           <input
             className="writeInput title"
             placeholder="Title"
             type="text"
-            onChange={e => setTitle(e.target.value)}
+            onChange={(e) => setTitle(e.target.value)}
             autoFocus={true}
           />
         </div>
@@ -56,17 +69,27 @@ export default function Write() {
             className="writeInput writeText"
             placeholder="Tell your story..."
             type="text"
-            onChange={e => setDesc(e.target.value)}
+            onChange={(e) => setDesc(e.target.value)}
             autoFocus={true}
           />
         </div>
         <button className="writeSubmit" type="submit">
           Publish
         </button>
-        <hr/>
-        {
-          error && <span style={{color:"red", marginRight:"8px", position:"relative", bottom:"30px",left:"500px"}}>please check the fields</span>
-        }
+        <hr />
+        {error && (
+          <span
+            style={{
+              color: "red",
+              marginRight: "8px",
+              position: "relative",
+              bottom: "30px",
+              left: "500px",
+            }}
+          >
+            please check the fields
+          </span>
+        )}
       </form>
     </div>
   );
--- server.js
+++ server.js
@@ -18,25 +18,26 @@ const catRoute = require("./routes/categoryRoute.js")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 // using the route
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/post", postRoute)
 app.use("/api/categories", catRoute)
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
-  app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("File has been uploaded");
-  });
 
 if(process.env.NODE_ENV === "local"){
     app.use(cors({
