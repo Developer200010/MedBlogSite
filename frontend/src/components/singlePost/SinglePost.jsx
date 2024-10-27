a1c5045bd5a5ad7b0a1af3ebfe7cb5e261b09de3
chandan vishwakarma <chandanvishw11002@gmail.com>
2024-10-27 21:18:46 +0530

    post updatation done

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -1,49 +1,83 @@
 import { Link, useLocation } from "react-router-dom";
 import "./singlePost.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../../context/Context";
 import axios from "axios";
-export default function SinglePost({data}) {
-  const PF = "http://localhost:5000/images/"
-  const {user} = useContext(Context)
-  const location = useLocation()
-  const path = location.pathname.split("/")[2]
-  console.log(path)
-  const handleDelete = async() =>{
+export default function SinglePost({ data }) {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
+  const PF = "http://localhost:5000/images/";
+  const { user } = useContext(Context);
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  
+  const handleDelete = async () => {
     try {
       await axios.delete("/api/post/" + path, {
-        data:{username: user.username}} );
+        data: { username: user.username },
+      });
       window.location.replace("/");
-
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
+  };
+
+  useEffect(()=>{
+    const getPost = async() =>{
+      const res= await axios.get("/api/post/" + path);
+      setTitle(res.data.title)
+      setDesc(res.data.desc)
+    }
+    getPost()
+  },[path])
+
+  const handleEdit = async() =>{
+    await axios.put(`/api/post/${data._id}`, {
+      username: user.username,
+      title,
+      desc 
+    })
+    window.location.reload();
   }
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        {data.photo === "" ? <img
-          className="singlePostImg"
-          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-          alt=""
-        /> : <img
-        className="singlePostImg"
-        src={PF + data.photo}
-        alt=""
-      /> }
-        
-        <h1 className="singlePostTitle">
-          {data.title}
+        {data.photo === "" ? (
+          <img
+            className="singlePostImg"
+            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+        ) : (
+          <img className="singlePostImg" src={PF + data.photo} alt="" />
+        )}
+        {updateMode ? (
+          <input value={title} className="singlePostTitleInput" onChange={(e)=> setTitle(e.target.value)} />
+        ) : (
+          <h1 className="singlePostTitle">
+            {data.title}
 
-            {data.username === user?.username &&
+            {data.username === user?.username && (
               <div className="singlePostEdit">
-              <i className="singlePostIcon far fa-edit">edit</i>
-              <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}>delete</i>
-            </div>
-            }
-          
-        
-        </h1>
+                <i
+                  className="singlePostIcon far fa-edit"
+                  onClick={(e) => setUpdateMode(true)}
+                >
+                  edit
+                </i>
+                <i
+                  className="singlePostIcon far fa-trash-alt"
+                  onClick={handleDelete}
+                >
+                  delete
+                </i>
+              </div>
+            )}
+          </h1>
+        )}
+
         <div className="singlePostInfo">
           <span>
             Author:
@@ -55,9 +89,14 @@ export default function SinglePost({data}) {
           </span>
           <span>{new Date(data.createdAt).toDateString()}</span>
         </div>
-        <p className="singlePostDesc">
-         {data.desc}
-        </p>
+        {updateMode ? (
+          <>
+          <textarea className="singlePostDescInput" onChange={(e)=> setDesc(e.target.value)}>{desc}</textarea>
+          <button className="updateBtn" onClick={handleEdit}>update</button>
+          </>
+        ) : (
+          <p className="singlePostDesc">{data.desc}</p>
+        )}
       </div>
     </div>
   );
